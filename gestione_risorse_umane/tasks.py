30bb74f798327a245b604f03b356b245ef508978
peo1 <peo1@uniurb.com>
2021-08-05 16:17:44 +0200

    feat: log

--- gestione_risorse_umane/tasks.py
+++ gestione_risorse_umane/tasks.py
@@ -11,13 +11,13 @@ from .models import *
 logger = logging.getLogger(__name__)
 
 def long_running_task(modeladmin, queryset):  
-    logger.info('Inizio long_running_task sincronizzazione dipendenti da CSA')
+    logger.error('Inizio long_running_task sincronizzazione dipendenti da CSA')
     num_sync = 0
     for i in queryset:
         try:
             if i.sync_csa():
                 num_sync += 1
-                logger.info('{} Dipendente sincronizzato da CSA'.format(i.matricola))
+                logger.error('{} Dipendente sincronizzato da CSA'.format(i.matricola))
             else:
                 logger.error('Sono incorsi errori nel sincronizzare {}'.format(i.__str__()))
         except Exception as e:        
@@ -25,4 +25,4 @@ def long_running_task(modeladmin, queryset):
             logger.error('Sono incorsi errori nel sincronizzare {}'.format(i.__str__()))
           
     if num_sync:
-       logger.info('{} Dipendenti sincronizzati da CSA'.format(num_sync))
+       logger.error('{} Dipendenti sincronizzati da CSA'.format(num_sync))
